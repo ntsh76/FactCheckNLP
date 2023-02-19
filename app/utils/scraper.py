9a9210fb33189afe9eee50ffb9eefa6ea84d98a9
Kavin Nguyen <ng.kavin97@gmail.com>
2023-02-18 23:36:13 -0800

    add google source scraping

---
+++ app/utils/scraper.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+from googlesearch import search
+
+def get_source_text(query: str) -> str:
+    # Search google
+    site = next(search(query))
+    # Scrape the resulting website from google
+    request = requests.get(site)
+    # Parse the scraped content
+    content = BeautifulSoup(request.content, features="html.parser")
+    content = " ".join([p.get_text() for p in content.find_all("p")])
+    return content
